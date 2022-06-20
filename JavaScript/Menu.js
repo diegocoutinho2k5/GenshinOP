663b78f323c39db0fd5c86dbaa1e5f406c7a21d3
Samuel LJG <samuelljg@outlook.com>
2022-06-20 02:25:11 -0300

    1123

--- JavaScript/Menu.js
+++ JavaScript/Menu.js
@@ -1,10 +1,15 @@
 
 function myFunction() {
     var x = document.getElementById("myLinks");
+    var menu = document.getElementById("menu")
     if (x.style.display === "block") {
       x.style.display = "none";
+      menu.style.display = "block";
+      ex.style.display = "none";
     } else {
       x.style.display = "block";
+menu.style.display = "none";
+ex.style.display = "block";
     }
   }
 

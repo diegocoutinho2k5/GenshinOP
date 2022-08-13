82d9b84c8b8fe0ea412389228c29d4c593f9305c
unknown <samuelljg@outlook.com>
2022-08-13 02:31:32 -0300

    fdsf

---
+++ JavaScript/darkmode.js
@@ -0,0 +1,25 @@
+
+  function darkmode(){
+    var SetTheme = document.body;
+    SetTheme.classList.toggle("dark")
+    var theme;
+    if(SetTheme.classList.contains("dark")){
+        theme = "DARK";
+    }else{
+        theme = "LIGHT";
+    }
+    // save to localStorage
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
+    // ensure you convert to JSON like i have done -----JSON.stringify(theme)
+    
+
+}
+
+setInterval(() => {
+    let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+    if(GetTheme === "DARK"){
+        document.body.classList = "dark";
+    }else{
+        document.body.classList = "";
+    }
+}, 5);

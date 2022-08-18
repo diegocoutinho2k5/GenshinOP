b63beab1f5bddca00f5e9d6e91bd59ba830545b7
unknown <samuelljg@outlook.com>
2022-08-18 08:54:46 -0300

    fdsfd22d

--- JavaScript/Menu.js
+++ JavaScript/Menu.js
@@ -1,6 +1,7 @@
 
 function myFunction() {
     var x = document.getElementById("myLinks");
+    
     var menu = document.getElementById("menu")
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -27,6 +28,9 @@ ex.style.display = "block";
     }
     
   }
+
+  
+
   function Scroll() {
     
 
